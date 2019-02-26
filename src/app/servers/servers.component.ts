ec47c99b7b4e338cb554c78c00360c956942f8cf
Oguzhan Dogan <dogan_oguzhan@hotmail.com>
2019-02-26 09:25:48 +0300

    CSS File test

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  //templateUrl: './servers.component.html',
-  template:`
-  <app-server></app-server>
-  <p>This is from template multiline</p>
-  `, 
+  templateUrl: './servers.component.html',
+  // template:`
+  // <app-server></app-server>
+  // <p>This is from template multiline</p>
+  // `, 
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
