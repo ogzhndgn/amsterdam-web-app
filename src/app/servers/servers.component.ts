f88f40c577bc4178bfeacaea04a73d4f130106e1
Oguzhan Dogan <dogan_oguzhan@hotmail.com>
2019-02-26 09:40:32 +0300

    selector methods

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: 'app-servers',
+  // selector: '.app-servers', 
+  selector: '[app-servers]',
   templateUrl: './servers.component.html',
   // template:`
   // <app-server></app-server>
